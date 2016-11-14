100a68314a459e8a7c3e3f855a8edbe8851f8b0c
Rylan <rylans@gmail.com>
2016-11-14 14:07:10 -0800

    more translation tests and sample corpus

--- TestFrEnTranslator.py
+++ TestFrEnTranslator.py
@@ -15,5 +15,23 @@ class TestFrEnTranslator(unittest.TestCase):
         p_il_to_the = self.translator.translation_probability('the', 'il')
         self.assertTrue(p_il_to_he > p_il_to_the)
 
+    def test_translate_corruption(self):
+        self.verify_translation('corruption', 'bribery')
+
+    def test_translate_sur(self):
+        self.verify_translation('sur', 'on')
+
+    def test_translate_quatorze(self):
+        self.verify_translation('quatorze', 'fourteen')
+
+    def test_translate_sans(self):
+        self.verify_translation('sans', 'without')
+
+    def test_translate_sous(self):
+        self.verify_translation('sous', 'under')
+
+    def verify_translation(self, french_word, english_word):
+        self.assertTrue(self.translator.translate_word(french_word) == english_word)
+
 if __name__ == '__main__':
     unittest.main()
