db058c6a76730edd89cf847f52bc0203feb2c7be
Rylan <rylans@gmail.com>
2016-11-15 10:31:55 -0800

    Fix bug in translation improvement loop. add test-specific bilingual text

--- FrEnTranslator.py
+++ FrEnTranslator.py
@@ -1,28 +1,37 @@
 # -*- coding: UTF-8 -*-
 '''French to English Translator'''
 
+from nltk.align import AlignedSent
+from nltk.align import IBMModel2
+from EnglishModel import EnglishModel
+
 class FrEnTranslator(object):
-    def __init__(self):
-        '''
-        fr_en_dict['maison']['house'] = 0.63
-        fr_en_dict['maison']['the'] = 0.02
-        '''
+    '''Translation module from French to English'''
+    def __init__(self, bitext_file_name):
         self.fr_en_dict = {}
         self.target_words = {}
         self.src_words = {}
-        self.inv_target_frequency = {}
 
-        self.learn_all([])
+        self.learn_all(bitext_file_name)
+        self.english = EnglishModel(['austen-emma.txt'])
 
-    def norm(self):
-        '''Normalize fr_en_dict from frequencies to probabilities'''
-        for src_word in self.fr_en_dict.keys():
-            src_word_sum = 0
-            for trg_word in self.fr_en_dict[src_word].keys():
-                src_word_sum += self.fr_en_dict[src_word][trg_word]
+    def norm_source_words(self):
+        '''Normalize source words from frequencies to probabilities'''
+        src_word_sum = 0
+        for src_word in self.src_words.keys():
+            src_word_sum += self.src_words[src_word]
 
-            for trg_word in self.fr_en_dict[src_word].keys():
-                self.fr_en_dict[src_word][trg_word] = self.fr_en_dict[src_word][trg_word]/float(src_word_sum)
+        for src_word in self.src_words.keys():
+            self.src_words[src_word] = self.src_words[src_word]/float(src_word_sum)
+
+    def norm_target_words(self):
+        '''Normalize source words from frequencies to probabilities'''
+        src_word_sum = 0
+        for src_word in self.target_words.keys():
+            src_word_sum += self.target_words[src_word]
+
+        for src_word in self.target_words.keys():
+            self.target_words[src_word] = self.target_words[src_word]/float(src_word_sum)
 
     def invert_targets(self):
         '''Invert target words
@@ -32,14 +41,14 @@ class FrEnTranslator(object):
         for trg_word in self.target_words.keys():
             self.inv_target_frequency[trg_word] = 1.0/(1+self.target_words[trg_word])
 
-    def learn_all(self, filenames):
+    def learn_all(self, filename):
         '''Learn lexicon from list of bilingual texts'''
-        #TODO read filenames
-        self.learn('texts/sample_en_fr.txt')
-        self.norm()
-        self.invert_targets()
+        self.learn(filename)
+        self.norm_source_words()
+        self.norm_target_words()
 
     def learn(self, filename):
+        bitext = []
         en_line = ''
         fr_line = ''
         with open(filename, 'r') as open_file:
@@ -49,12 +58,23 @@ class FrEnTranslator(object):
                 if "FR " in line:
                     fr_line = line
                 if en_line != '' and fr_line != '':
+                    bitext.append(AlignedSent(self.ws(fr_line), self.ws(en_line)))
                     self.learn_aligned_sentence(en_line, fr_line)
                     en_line, fr_line = '', ''
+        self.ibm2 = IBMModel2(bitext, 2)
+        '''
+        print ibm2.translation_table
+        print ibm2.translation_table['avec']
+        print ibm2.translation_table['moi']
+        '''
+
+    def ws(self, source):
+        return [w.lower() for w in source.split(' ')[1:]]
 
     def learn_aligned_sentence(self, target, source):
         source_words = [w.lower() for w in source.split(' ')[1:-1]]
         target_words = [w.lower() for w in target.split(' ')[1:-1]]
+
         for source_word in source_words:
             for target_word in target_words:
                 if self.fr_en_dict.get(source_word) == None:
@@ -75,7 +95,7 @@ class FrEnTranslator(object):
 
     def translate_word(self, src_word, top=1):
         if self.fr_en_dict.get(src_word) == None:
-            return '[no-translation]'
+            return ['[no-translation]']
 
         candidates = []
         for trg_word in self.fr_en_dict[src_word]:
@@ -92,26 +112,85 @@ class FrEnTranslator(object):
             return 0
         if self.fr_en_dict[src_word].get(trg_word) == None:
             return 0
-        return self.fr_en_dict[src_word][trg_word] * self.inv_target_frequency[trg_word]
+        return self.ibm2.translation_table[src_word][trg_word] * self.src_words[src_word]
+
+    def translate(self, source_sentence):
+        '''Translate source sentence to English'''
+        src_words = source_sentence.split(' ')
+        local_dict = {}
+        for src_word in src_words:
+            local_dict[src_word] = self.translate_word(src_word, 8)
+            print src_word + " " + str(local_dict[src_word])
+
+        possible_translation = []
+        for src_word in src_words:
+            possible_translation.append(local_dict[src_word][0])
+
+
+        px1 = self.english.avg_perplexity(possible_translation)
+        px2 = px1
+        improvement = 1
+        alternate = ' '.join(possible_translation)
+        while (improvement > 0.03):
+            ix = self.most_perplexing(possible_translation)
+            print improvement, ix
+            if ix == 0:
+                break
+            try:
+                alternate = ' '.join(possible_translation)
+                possible_translation[ix] = self.translate_word(src_words[ix], 2)[1]
+            except IndexError:
+                return alternate
+
+            px1 = px2
+            px2 = self.english.avg_perplexity(possible_translation)
+            improvement = px1 - px2
+            if improvement < 0:
+                return alternate
+
+        return ' '.join(possible_translation)
+
+    def argmin(self, lst):
+        minval = lst[0]
+        minix = 0
+        for i in range(len(lst)):
+            if lst[i] < minval:
+                minval = lst[i]
+                minix = i
+        return minix
+
+    def most_perplexing(self, sentence):
+        '''Compute the index of the sentence that increases perplexity the most'''
+        interesting_index = len(sentence) - 1
+
+        px_array = []
+        last_px = 0
+        for k in range(len(sentence) - 1):
+            sen = sentence[:k+1]
+            px = self.english.avg_perplexity(sen)
+            marginal = px - last_px
+            if marginal > 0.02:
+                interesting_index = k
+            last_px = px
+            px_array.append(marginal)
+
+        print px_array
+        return self.argmin(px_array)
 
 def demo():
-    translator = FrEnTranslator()
+    '''
+    translator = FrEnTranslator('texts/sample_en_fr.txt')
 
-    '''
-    sen = 'soit de provoquer ou de faciliter la perpétration d’une infraction ,'.split(' ')
-    for word in sen:
-        print word + " " + str(translator.translate_word(word, 8))
+    phrase2 = "corruption de fonctionnaires avait un avis de notre femme ."
+    print translator.translate(phrase2)
+    print
 
+    phrase3 = "sa mère avait les choix et elle avait la fille ."
+    print translator.translate(phrase3)
     '''
-    sen2 = "personne avec l’intention de offre qui .".split()
-    for word in sen2:
-        print word + " " + str(translator.translate_word(word, 8))
 
-    '''
-    for src_word in translator.src_words:
-        if translator.src_words[src_word] > 2:
-            print src_word
-    '''
+    translator = FrEnTranslator('texts/test_en_fr.txt')
+    print translator.translate('elles dansent et elles sont morts .')
 
 if __name__ == '__main__':
     demo()
--- TestFrEnTranslator.py
+++ TestFrEnTranslator.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import unittest
 
 from FrEnTranslator import FrEnTranslator
@@ -5,30 +6,19 @@ from FrEnTranslator import FrEnTranslator
 class TestFrEnTranslator(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.translator = FrEnTranslator()
+        self.translator = FrEnTranslator('texts/test_en_fr.txt')
 
-    def test_source_il_translations(self):
-        p_il_to_he = self.translator.translation_probability('he', 'il')
-        self.assertTrue(p_il_to_he > 0)
-        p_il_to_she = self.translator.translation_probability('she', 'il')
-        self.assertTrue(p_il_to_he > p_il_to_she)
-        p_il_to_the = self.translator.translation_probability('the', 'il')
-        self.assertTrue(p_il_to_he > p_il_to_the)
+    def test_translate_garcon(self):
+        self.verify_translation('garçon', 'boy')
 
-    def test_translate_corruption(self):
-        self.verify_translation('corruption', 'bribery')
+    def test_translate_dansent(self):
+        self.verify_translation('dansent', 'dance')
 
-    def test_translate_sur(self):
-        self.verify_translation('sur', 'on')
+    def test_translate_elles_dansent(self):
+        self.verify_full_translation('elles dansent et elles sont morts .', 'they dance and they are dead .')
 
-    def test_translate_quatorze(self):
-        self.verify_translation('quatorze', 'fourteen')
-
-    def test_translate_sans(self):
-        self.verify_translation('sans', 'without')
-
-    def test_translate_sous(self):
-        self.verify_translation('sous', 'under')
+    def verify_full_translation(self, french, english):
+        self.assertTrue(self.translator.translate(french) == english)
 
     def verify_translation(self, french_word, english_word):
         self.assertTrue(self.translator.translate_word(french_word) == english_word)
