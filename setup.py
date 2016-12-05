ee5c5490f87de375590c8ebf91befd32c6fd4059
Rylan <rylans@gmail.com>
2016-12-05 13:48:01 -0800

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='pytranslate',
+        version='0.0.5',
+        description='Machine learning and translation library',
+        author_email='rylans@gmail.com',
+        license='Apache 2.0',
+        py_modules=['pytranslate'],
+        install_requires=[
+        ],
+)
